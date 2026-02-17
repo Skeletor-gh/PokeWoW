7b0a611b1c4b05d3c20247cac9d8432c0b5e9bc6
Skeletor <jmdsmg@gmail.com>
2026-02-17 15:58:30 +0000

    New Sounds and Keeping

--- MusicTracks.lua
+++ MusicTracks.lua
@@ -9,6 +9,36 @@ ns.MusicTracks = {
             name = "Pokemon",
             path = "Interface\\AddOns\\PokeWoW\\Tracks\\pokemon.mp3",
             length = 24,
-        }        
+        },
+		{
+            name = "Mortal Kombat Menu",
+            path = "Interface\\AddOns\\PokeWoW\\Tracks\\MortalKombat_menu.mp3",
+            length = 22,
+        },
+		{
+            name = "Street Fighter Guile",
+            path = "Interface\\AddOns\\PokeWoW\\Tracks\\StreetFighter_guile.mp3",
+            length = 33,
+        },
+		{
+            name = "Street Fighter Ken",
+            path = "Interface\\AddOns\\PokeWoW\\Tracks\\StreetFighter_ken.mp3",
+            length = 66,
+        },
+		{
+            name = "Street Fighter II Theme",
+            path = "Interface\\AddOns\\PokeWoW\\Tracks\\StreetFighter_main.mp3",
+            length = 26,
+        },
+		{
+            name = "Mortal Kombat Menu",
+            path = "Interface\\AddOns\\PokeWoW\\Tracks\\MortalKombat_menu.mp3",
+            length = 22,
+        },
+		{
+            name = "Street Fighter Ryu",
+            path = "Interface\\AddOns\\PokeWoW\\Tracks\\StreetFighter_ryu.mp3",
+            length = 29,
+        }
     },
 }
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A World of Warcraft addon focusing on QoL and UX for Pet Battles. Gotta catch'em
 - Added a welcome options pane and dedicated sub-options pane for Pet Battle Music.
 - Added Pet Battle music replacement with modes: no music, single track loop, sequential, and random.
 - Added configurable user playlist declaration in `MusicTracks.lua`.
+- Added a set of soundtracks for the player's enjoyment.
+
